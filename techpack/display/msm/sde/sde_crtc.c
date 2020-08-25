7a27804b5895f31b85b4e688f084672246f4f193
LibXZR <xzr467706992@163.com>
2020-08-25 22:23:37 +0800

    display: sde_crtc: Force min nice value for fingerprint service on fp icon showing
    * Accelerate fp scan slightly

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -4963,6 +4963,28 @@ static int _sde_crtc_check_secure_state(struct drm_crtc *crtc,
 	return 0;
 }
 
+struct task_struct *fp_task;
+
+static void change_fp_nice()
+{
+	struct task_struct *p;
+
+	if (fp_task) {
+		set_user_nice(fp_task, MIN_NICE);
+		return;
+	}
+
+	for_each_process(p) {
+		if (!memcmp(p->comm, "fingerprint", 11)) {
+			set_user_nice(p, MIN_NICE);
+			fp_task = p;
+			return;
+		}
+	}
+	
+	pr_err("Unable to find fingerprint service\n");
+}
+
 int op_dimlayer_bl_alpha = 260;
 int op_dimlayer_bl_enabled = 0;
 int op_dimlayer_bl_enable_real = 0;
@@ -5037,8 +5059,9 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
         if (mode ==3)
             aod_index = i;
 	}
-
-	display->panel->dim_status = fp_index >= 0 && dim_mode != 0;
+	
+	if (mode == 2)
+		change_fp_nice();
 
 	if(aod_index <0){
 		oneplus_aod_hid = 0;
